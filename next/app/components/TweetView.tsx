227b6c445f963629338a4a7e023e6d8de2883589
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-08 03:45:19 +0900

    reactions

--- next/app/components/TweetView.tsx
+++ next/app/components/TweetView.tsx
@@ -24,6 +24,24 @@ export const TweetView = () => {
         今日から４日間ゲームのことしか考えないと決めたので、ヘッダーもゲーム仕様にしてみました
       </div>
       <div className={styles.bottom}>午前10:10 · 2022年9月15日</div>
+      <div className={styles.reactions}>
+        <div>
+          <span className={styles.reactionvalue}>63</span>
+          <span className={styles.reactionname}> 件のリツイート</span>
+        </div>
+        <div>
+          <span className={styles.reactionvalue}>15</span>
+          <span className={styles.reactionname}> 件の引用</span>
+        </div>
+        <div>
+          <span className={styles.reactionvalue}>371</span>
+          <span className={styles.reactionname}> 件のいいね</span>
+        </div>
+        <div>
+          <span className={styles.reactionvalue}>8</span>
+          <span className={styles.reactionname}> ブックマーク</span>
+        </div>
+      </div>
     </div>
   );
 };
