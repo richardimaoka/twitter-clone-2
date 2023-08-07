5654c0e018446420c730aa789d105503c1349ed6
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-08 03:21:41 +0900

    structure and css

--- next/app/components/Tweet.tsx
+++ next/app/components/Tweet.tsx
@@ -1,17 +1,28 @@
+import styles from "./style.module.css";
+import Image from "next/image";
+
 export const TweetView = () => {
+  //https://twitter.com/TwitterJP/status/1570218396775481344
   return (
-    <div className="tweet">
-      <div className="tweet-header">
-        <div>X</div>
+    <div className={styles.tweet}>
+      <div className={styles.header}>
+        <div>
+          <Image
+            src="/images/twitter.jpg"
+            width={40}
+            height={40}
+            alt={"user profile pic"}
+          />
+        </div>
         <div>
           <div>Japan</div>
           <div>&#064;TwitterJP</div>
         </div>
       </div>
-      <div className="tweet-body">
+      <div className={styles.body}>
         今日から４日間ゲームのことしか考えないと決めたので、ヘッダーもゲーム仕様にしてみました
       </div>
-      <div className="tweet-bottom">午前10:10 · 2022年9月15日</div>
+      <div className={styles.bottom}>午前10:10 · 2022年9月15日</div>
     </div>
   );
 };
