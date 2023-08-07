4d8be9c3b6f1dd48aba50c96c248bb1605adcd40
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-08 03:08:53 +0900

    structure

--- next/app/components/Tweet.tsx
+++ next/app/components/Tweet.tsx
@@ -1,13 +1,17 @@
 export const TweetView = () => {
   return (
-    <div>
-      <div>X</div>
-      <div>Japan</div>
-      <div>&#064;TwitterJP</div>
-      <div>
+    <div className="tweet">
+      <div className="tweet-header">
+        <div>X</div>
+        <div>
+          <div>Japan</div>
+          <div>&#064;TwitterJP</div>
+        </div>
+      </div>
+      <div className="tweet-body">
         今日から４日間ゲームのことしか考えないと決めたので、ヘッダーもゲーム仕様にしてみました
       </div>
-      <div>午前10:10 · 2022年9月15日</div>
+      <div className="tweet-bottom">午前10:10 · 2022年9月15日</div>
     </div>
   );
 };
