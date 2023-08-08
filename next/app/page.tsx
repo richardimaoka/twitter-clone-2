6fc4c47b2facab83904fff3b223ad0af818aa347
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-08 17:57:07 +0900

    page.tsx

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,9 +1,19 @@
 import { TweetColumn } from "./components/TweetColumn";
+import { getClient } from "@/libs/apolloClient";
+import { graphql } from "@/libs/gql";
 
-export default function Home() {
-  return (
-    <main>
-      <TweetColumn />
-    </main>
-  );
+const queryDefinition = graphql(/* GraphQL */ `
+  query PageQuery {
+    tweet {
+      ...TweetColumnFragment
+    }
+  }
+`);
+
+export default async function Home() {
+  const { data } = await getClient().query({
+    query: queryDefinition,
+  });
+
+  return <main>{data.tweet && <TweetColumn fragment={data.tweet} />}</main>;
 }
