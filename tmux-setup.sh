6c3c20bae4a588702dc932d5f9456d1cf949c797
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-08 16:01:09 +0900

    tmux-setup.sh

---
+++ tmux-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+PWD=$(pwd)
+
+SESSION=$(basename "$PWD")
+
+tmux new-session -s "$SESSION" -d
+tmux send-keys -t "$SESSION:0.0" '(cd gqlgen && gow -e=gql run github.com/99designs/gqlgen generate)' C-m
+
+tmux split-window -v -t "$SESSION"
+tmux select-layout even-vertical   # to avoid 'no space for new pane'
+tmux send-keys -t "$SESSION:0.1" '(cd gqlgen && gow -e=go,json run .)' C-m
+
+tmux split-window -v -t "$SESSION"
+tmux select-layout even-vertical   # to avoid 'no space for new pane'
+tmux send-keys -t "$SESSION:0.2" '(cd next && npm run dev)' C-m
+
+tmux attach -t "$SESSION"
