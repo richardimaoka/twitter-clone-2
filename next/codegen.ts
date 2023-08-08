92934a345cd369e6f170a720fc5855c8a40f7310
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-08 17:02:46 +0900

    modify codegen.ts

--- next/codegen.ts
+++ next/codegen.ts
@@ -1,16 +1,23 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
   schema: "../gqlgen/graph/schema.graphqls",
-  documents: "\"app/**/*.tsx\"",
+  documents: ["app/**/*.tsx"],
   generates: {
     "libs/gql/": {
       preset: "client",
-      plugins: []
-    }
-  }
+      plugins: [],
+      config: {
+        // avoidOptionals: true,
+        nonOptionalTypename: true, //__typename should always exist
+        enumsAsTypes: true,
+        dedupeFragments: true, //GraphQL】 There can be only one fragment named XxxFragment error - https://zenn.dev/bicstone/articles/graphql-dedupe-fragments, https://github.com/dotansimha/graphql-code-generator/issues/8670
+      },
+    },
+  },
+  watch: ["app/**/*.tsx", "../gqlgen/graph/schema.graphqls"],
+  hooks: { afterOneFileWrite: ["npx prettier --write"] },
 };
 
 export default config;
