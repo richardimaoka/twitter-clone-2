e4f2eec57dad188b5b6c1d4db2f1c03d3d9a6d1e
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-08 17:56:56 +0900

    TweetColumn

--- next/app/components/TweetColumn.tsx
+++ next/app/components/TweetColumn.tsx
@@ -1,10 +1,24 @@
 import { TweetView } from "./TweetView";
 import styles from "./style.module.css";
 
-export const TweetColumn = () => {
+import { FragmentType, graphql, useFragment } from "@/libs/gql";
+
+const fragmentDefinition = graphql(`
+  fragment TweetColumnFragment on Tweet {
+    ...TweetFragment
+  }
+`);
+
+interface TweetColumnProps {
+  fragment: FragmentType<typeof fragmentDefinition>;
+}
+
+export const TweetColumn = (props: TweetColumnProps) => {
+  const fragment = useFragment(fragmentDefinition, props.fragment);
+
   return (
     <div className={styles.column}>
-      <TweetView />
+      <TweetView fragment={fragment} />
     </div>
   );
 };
