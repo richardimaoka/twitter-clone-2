0b3cb65faea703b4c5254621e003392ab349874c
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-17 22:23:46 +0900

    CORS

--- gqlgen/go.mod
+++ gqlgen/go.mod
@@ -4,6 +4,8 @@ go 1.18
 
 require (
 	github.com/99designs/gqlgen v0.17.36
+	github.com/go-chi/chi v1.5.4
+	github.com/rs/cors v1.9.0
 	github.com/vektah/gqlparser/v2 v2.5.8
 )
 
--- gqlgen/server.go
+++ gqlgen/server.go
@@ -7,7 +7,9 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
 	"github.com/richardimaoka/twitter-clone-2/gqlgen/graph"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
@@ -18,11 +20,21 @@ func main() {
 		port = defaultPort
 	}
 
+	router := chi.NewRouter()
+
+	// Add CORS middleware around every request
+	// See https://github.com/rs/cors for full option listing
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
