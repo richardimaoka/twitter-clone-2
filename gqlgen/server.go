e0966704417e88cda9af34612dc1eefcba043289
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-08 09:01:17 +0900

    command line args server

--- gqlgen/server.go
+++ gqlgen/server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -25,7 +27,7 @@ type City struct {
 	Regions    []string `firestore:"regions,omitempty"`
 }
 
-func main() {
+func server() {
 	app, err := firebase.NewApp(context.Background(), nil)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
@@ -83,4 +85,26 @@ func main() {
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
+}
+
+func main() {
+	serverSubCmd := flag.NewFlagSet("server", flag.ExitOnError)
+	seedSubCmd := flag.NewFlagSet("seed", flag.ExitOnError)
+
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'server' or 'seed' subcommands")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "server":
+		serverSubCmd.Parse(os.Args[2:])
+		server()
+	case "seed":
+		seedSubCmd.Parse(os.Args[2:])
+	default:
+		fmt.Println("expected 'server' or 'seed' subcommands")
+		os.Exit(1)
+	}
+
 }
