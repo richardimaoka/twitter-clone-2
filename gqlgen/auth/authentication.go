d8bcc24f8138b263904e650c071b24810d894a91
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-09 15:49:54 +0900

    inject dbClient and authClient

--- gqlgen/auth/authentication.go
+++ gqlgen/auth/authentication.go
@@ -2,9 +2,13 @@ package auth
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"net/http"
 
 	"cloud.google.com/go/firestore"
+	"firebase.google.com/go/v4/auth"
+
 	"github.com/richardimaoka/twitter-clone-2/gqlgen/database"
 )
 
@@ -22,30 +26,35 @@ type User struct {
 	IsAdmin bool
 }
 
-func validateAndGetUserID(c *http.Cookie) (string, error) {
+func validateAndGetUserID(ctx context.Context, authClient *auth.Client, idToken string) (string, error) {
+	token, err := authClient.VerifyIDToken(ctx, idToken)
+	if err != nil {
+		log.Fatalf("error verifying ID token: %v\n", err)
+		return "", fmt.Errorf("invalid credentials")
+	}
+
+	log.Printf("Verified ID token: %v\n", token)
 	return "", nil
 }
 
 // Middleware decodes the share session cookie and packs the session into context
-func Middleware(client *firestore.Client) func(http.Handler) http.Handler {
+func AuthMiddleware(dbClient *firestore.Client, authClient *auth.Client) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			c, err := r.Cookie("auth-cookie")
-
-			// Allow unauthenticated users in
-			if err != nil || c == nil {
-				next.ServeHTTP(w, r)
+			if err != nil {
+				http.Error(w, "Invalid cookie", http.StatusForbidden)
 				return
 			}
 
-			userId, err := validateAndGetUserID(c)
+			userId, err := validateAndGetUserID(r.Context(), authClient, c.Value)
 			if err != nil {
 				http.Error(w, "Invalid cookie", http.StatusForbidden)
 				return
 			}
 
 			// get the user from the database
-			user, err := database.GetUser(r.Context(), client, userId)
+			user, err := database.GetUser(r.Context(), dbClient, userId)
 
 			// put it in context
 			ctx := context.WithValue(r.Context(), userCtxKey, user)
