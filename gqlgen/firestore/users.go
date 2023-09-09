b73349d97094a21502f4b62cbc8719fa9b0a1a3e
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-09 14:53:34 +0900

    get user from firestore

--- gqlgen/firestore/users.go
+++ gqlgen/firestore/users.go
@@ -1,7 +1,32 @@
 package firestore
 
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/firestore"
+)
+
 type User struct {
 	UserName       string `firestore:"userName"`
 	UserId         string `firestore:"userId"`
 	ProfilePicture string `firestore:"profilePicture"`
+}
+
+// Get user from firestore
+func GetUserFromFirestore(ctx context.Context, client *firestore.Client, userId string) (*User, error) {
+	userRef := client.Doc("users/" + userId)
+
+	docSnap, err := userRef.Get(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user: %v", err)
+	}
+
+	var user User
+	err = docSnap.DataTo(&user)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert user: %v", err)
+	}
+
+	return &user, nil
 }
