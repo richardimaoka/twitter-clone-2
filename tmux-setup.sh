2a9386142010f86b815531ff1ba366a24c2eb951
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-14 00:00:03 +0900

    add server subcommand to tmux

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -9,7 +9,7 @@ tmux send-keys -t "$SESSION:0.0" '(cd gqlgen && gow -e=graphqls run github.com/9
 
 tmux split-window -v -t "$SESSION"
 tmux select-layout even-vertical   # to avoid 'no space for new pane'
-tmux send-keys -t "$SESSION:0.1" '(cd gqlgen && gow -e=go,json run .)' C-m
+tmux send-keys -t "$SESSION:0.1" '(cd gqlgen && gow -e=go,json run . server)' C-m
 
 tmux split-window -v -t "$SESSION"
 tmux select-layout even-vertical   # to avoid 'no space for new pane'
