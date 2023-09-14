e6761b051eb69aa624b6b060f93a5a58021d7b38
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-14 18:49:00 +0900

    icon styling

--- next/components/rightpane/PromotedHashTag.tsx
+++ next/components/rightpane/PromotedHashTag.tsx
@@ -1,4 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./style.module.css";
+import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 export function PromotedHashTag() {
   return (
@@ -8,6 +10,10 @@ export function PromotedHashTag() {
         『モンスターハンターNow』リアル狩猟解禁！
       </div>
       <div className={styles.promotedHashTagAuthor}>
+        <FontAwesomeIcon
+          className={styles.promotedHashTagAuthorIcon}
+          icon={faUpRightFromSquare}
+        />
         Promoted by モンスターハンターNow 公式
       </div>
     </div>
