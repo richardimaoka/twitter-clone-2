ae22814124058df4d270b51a5fdef58c0ffd9494
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-15 07:56:57 +0900

    validation logic in route handler

--- next/app/login/handler/route.ts
+++ next/app/login/handler/route.ts
@@ -1,63 +1,87 @@
 import { NextResponse } from "next/server";
+// Firebase Admin SDK as route handler is purely on server-side
+import { getApp, getApps, initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
 
-type IdTokenRequest = {
+type LoginRequestBody = {
   idToken: string;
+  // csrfToken: string;
 };
 
-function hasIdToken(jsonBody: any): jsonBody is IdTokenRequest {
+function isValidRequest(jsonBody: any): jsonBody is LoginRequestBody {
   return "idToken" in jsonBody;
 }
 
 export async function GET(request: Request) {
   console.log("/login/handler/route.ts GET");
-
   try {
-    const json = await request.json();
-    if (hasIdToken(json)) {
-      const idToken = json.idToken;
-    } else {
-      console.log("invalid login request - missing idToken in json body");
+    // Firebase Admin SDK as route handler is purely on server-side
+    const appAlreadyExists = getApps().length === 0;
+    const app = appAlreadyExists ? initializeApp() : getApp();
+    const auth = getAuth(app);
+
+    try {
+      const json = await request.json();
+      if (isValidRequest(json)) {
+        // https://firebase.google.com/docs/auth/admin/manage-cookies#create_session_cookie
+        // Get the ID token passed and the CSRF token.
+        const idToken = json.idToken;
+
+        // const csrfToken =json.csrfToken
+        // // Guard against CSRF attacks.
+        // if (csrfToken !== req.cookies.csrfToken) {
+        //   res.status(401).send("UNAUTHORIZED REQUEST!");
+        //   return;
+        // }
+
+        // Set session expiration to 5 days.
+        const expiresIn = 60 * 60 * 24 * 5 * 1000;
+
+        // Create the session cookie. This will also verify the ID token in the process.
+        // The session cookie will have the same claims as the ID token.
+        // To only allow session cookie setting on recent sign-in, auth_time in ID token
+        // can be checked to ensure user was recently signed in before creating a session cookie.
+
+        try {
+          const sessionCookie = await auth.createSessionCookie(idToken, {
+            expiresIn,
+          });
+
+          // Set cookie policy for session cookie.
+          const options = { maxAge: expiresIn, httpOnly: true, secure: true };
+          let response = NextResponse.next();
+          response.cookies.set("session", sessionCookie, options);
+
+          return NextResponse.json({ status: "success" });
+        } catch (e) {
+          console.log(e);
+          console.log("Failed to create session cookie");
+          return NextResponse.json(
+            { error: "internal server error" },
+            { status: 500 }
+          );
+        }
+      } else {
+        console.log("invalid login request - missing idToken in json body");
+        return NextResponse.json(
+          { error: "invalid login request" },
+          { status: 401 }
+        );
+      }
+    } catch (e) {
+      console.log("invalid login request - missing json body");
       return NextResponse.json(
         { error: "invalid login request" },
         { status: 401 }
       );
     }
   } catch (e) {
-    console.log("invalid login request - missing json body");
+    console.log(e);
+    console.log("Firebase Admin SDK initialization error");
+
     return NextResponse.json(
-      { error: "invalid login request" },
-      { status: 401 }
+      { error: "internal server error" },
+      { status: 500 }
     );
   }
-
-  // https://firebase.google.com/docs/auth/admin/manage-cookies#create_session_cookie
-  // Get the ID token passed and the CSRF token.
-  // const idToken = request.body.idToken.toString();
-  // const csrfToken = req.body.csrfToken.toString();
-  // // Guard against CSRF attacks.
-  // if (csrfToken !== req.cookies.csrfToken) {
-  //   res.status(401).send("UNAUTHORIZED REQUEST!");
-  //   return;
-  // }
-  // // Set session expiration to 5 days.
-  // const expiresIn = 60 * 60 * 24 * 5 * 1000;
-  // // Create the session cookie. This will also verify the ID token in the process.
-  // // The session cookie will have the same claims as the ID token.
-  // // To only allow session cookie setting on recent sign-in, auth_time in ID token
-  // // can be checked to ensure user was recently signed in before creating a session cookie.
-  // getAuth()
-  //   .createSessionCookie(idToken, { expiresIn })
-  //   .then(
-  //     (sessionCookie) => {
-  //       // Set cookie policy for session cookie.
-  //       const options = { maxAge: expiresIn, httpOnly: true, secure: true };
-  //       res.cookie("session", sessionCookie, options);
-  //       res.end(JSON.stringify({ status: "success" }));
-  //     },
-  //     (error) => {
-  //       res.status(401).send("UNAUTHORIZED REQUEST!");
-  //     }
-  //   );
-
-  return NextResponse.json({ hello: "world" });
 }
