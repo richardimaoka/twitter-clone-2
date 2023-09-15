3fddfe852ef767b74b40f96247c5a3279c8e1f77
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-16 05:16:43 +0900

    remove unnecessary firebase config

--- next/app/home/page.tsx
+++ next/app/home/page.tsx
@@ -6,12 +6,6 @@ import { redirect } from "next/navigation";
 import { TweetTimelineView } from "../../components/timeline/TweetTimeline";
 import styles from "./style.module.css";
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-};
-
 export default async function Page() {
   const cookieStore = cookies();
   const sessionCookie = cookieStore.get("session");
@@ -27,7 +21,7 @@ export default async function Page() {
         <LeftPane />
       </section>
       <main>
-        <TweetTimelineView firebaseConfig={firebaseConfig} />
+        <TweetTimelineView />
       </main>
       <section className={styles.rightPane}>
         <RightPane />
