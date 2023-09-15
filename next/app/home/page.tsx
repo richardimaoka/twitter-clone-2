9b721b5ac6c6ccec2f71ed8ef85649a521ed4eca
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-16 04:23:43 +0900

    non-login redirect

--- next/app/home/page.tsx
+++ next/app/home/page.tsx
@@ -1,12 +1,10 @@
 import { LeftPane } from "@/components/leftpane/LeftPane";
 import { RightPane } from "@/components/rightpane/RightPane";
-import { getApp, getApps, initializeApp } from "firebase-admin/app";
-import { getAuth } from "firebase-admin/auth";
+import { isLoggedInServerSide } from "@/libs/serverAuth";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { TweetTimelineView } from "../../components/timeline/TweetTimeline";
 import styles from "./style.module.css";
-import { decode } from "punycode";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
@@ -15,23 +13,13 @@ const firebaseConfig = {
 };
 
 export default async function Page() {
-  // const cookieStore = cookies();
-  // const idToken = cookieStore.get("idToken");
+  const cookieStore = cookies();
+  const sessionCookie = cookieStore.get("session");
 
-  // if (!idToken) redirect("/login");
-
-  // const appAlreadyExists = getApps().length === 0;
-  // const app = appAlreadyExists ? initializeApp() : getApp();
-  // const auth = getAuth(app);
-
-  // try {
-  //   const decoded = await auth.verifyIdToken(idToken.value);
-  // } catch (error) {
-  //   console.log(
-  //     `redirecting to /login as the id token from cookie = '${idToken.value}' is invalid: ${error}`
-  //   );
-  //   redirect("/login");
-  // }
+  const isLoggedIn = await isLoggedInServerSide(sessionCookie?.value);
+  if (!isLoggedIn) {
+    redirect("/login");
+  }
 
   return (
     <div className={styles.home}>
--- next/libs/serverAuth.ts
+++ next/libs/serverAuth.ts
@@ -1,6 +1,6 @@
 import { getApp, getApps, initializeApp } from "firebase-admin/app";
 import { Auth, getAuth } from "firebase-admin/auth";
-import { ErrorResult } from "./errors";
+import { ErrorResult, isErrorResult } from "./errors";
 
 export function getFirebaseAuth(): Auth | ErrorResult {
   try {
@@ -14,5 +14,51 @@ export function getFirebaseAuth(): Auth | ErrorResult {
       error: "Firebase Admin SDK initialization error",
       detail: `${e}`,
     };
+  }
+}
+
+async function verifySessionCookie(
+  firebaseAuth: Auth,
+  sessionCookie: string
+): Promise<boolean | ErrorResult> {
+  // try-catch at the root - (i.e.) no throw
+  try {
+    // upon verificatoin failure, it will throw
+    await firebaseAuth.verifySessionCookie(
+      sessionCookie,
+      true /** checkRevoked */
+    );
+
+    return true;
+  } catch (e) {
+    return {
+      kind: "ErrorResult",
+      error: "Failed to verify session cookie",
+      detail: `${e}`,
+    };
+  }
+}
+
+// 1. Return true if logged in
+// 2. Return false if not logged in
+// 3. Throw, to render error page on Next.js route, upon internal error (e.g. Firebase Admin SDK initialization error)
+export async function isLoggedInServerSide(
+  sessionCookie?: string
+): Promise<boolean> {
+  // Firebase Admin SDK as route handler is purely on server-side
+  const auth = getFirebaseAuth();
+  if (isErrorResult(auth)) {
+    console.log(auth.error);
+    console.log(auth.detail);
+    throw new Error("internal error"); // can't proceed, show error and haltion error)
+  }
+
+  if (!sessionCookie) return false;
+
+  const verified = await verifySessionCookie(auth, sessionCookie);
+  if (isErrorResult(verified)) {
+    return false;
+  } else {
+    return verified;
   }
 }
