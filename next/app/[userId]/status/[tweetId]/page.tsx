432a5f158c0dbe5e1572365b5c021b75e99971d5
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-17 04:42:51 +0900

    layout

--- next/app/[userId]/status/[tweetId]/page.tsx
+++ next/app/[userId]/status/[tweetId]/page.tsx
@@ -1,3 +1,17 @@
+import { LeftPane } from "@/components/leftpane/LeftPane";
+import { RightPane } from "@/components/rightpane/RightPane";
+import styles from "./style.module.css";
+
 export default function Page() {
-  return <div>tweet</div>;
+  return (
+    <div className={styles.home}>
+      <section className={styles.leftPane}>
+        <LeftPane />
+      </section>
+      <main></main>
+      <section className={styles.rightPane}>
+        <RightPane />
+      </section>
+    </div>
+  );
 }
