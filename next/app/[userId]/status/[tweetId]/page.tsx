e0b58754f12775d615e34eddd737bf7bc2b5379a
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-17 04:45:48 +0900

    tweet compoinent

--- next/app/[userId]/status/[tweetId]/page.tsx
+++ next/app/[userId]/status/[tweetId]/page.tsx
@@ -2,13 +2,27 @@ import { LeftPane } from "@/components/leftpane/LeftPane";
 import { RightPane } from "@/components/rightpane/RightPane";
 import styles from "./style.module.css";
 
-export default function Page() {
+import { TweetColumn } from "@/components/thread/TweetColumn";
+import { graphql } from "@/libs/gql";
+import { request } from "graphql-request";
+// import { print } from "graphql";
+
+const queryDefinition = graphql(/* GraphQL */ `
+  query RootPageQuery {
+    tweet {
+      ...TweetColumnFragment
+    }
+  }
+`);
+
+export default async function Page() {
+  const data = await request("http://localhost:8080/query", queryDefinition);
   return (
     <div className={styles.home}>
       <section className={styles.leftPane}>
         <LeftPane />
       </section>
-      <main></main>
+      <main>{data.tweet && <TweetColumn fragment={data.tweet} />}</main>
       <section className={styles.rightPane}>
         <RightPane />
       </section>
