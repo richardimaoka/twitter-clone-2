2019d23107c5296aca4b569b369c67ea4c401f0c
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-20 09:37:38 +0900

    is logged in check at /

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,8 +1,10 @@
 import { graphql } from "@/libs/gql";
+import { isLoggedInServerSide } from "@/libs/serverAuth";
+import { request } from "graphql-request";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import { TweetColumn } from "../components/thread/TweetColumn";
 // import { print } from "graphql";
-
-import { request } from "graphql-request";
 
 const queryDefinition = graphql(/* GraphQL */ `
   query RootPageQuery {
@@ -11,6 +13,14 @@ const queryDefinition = graphql(/* GraphQL */ `
 `);
 
 export default async function Home() {
+  const cookieStore = cookies();
+  const sessionCookie = cookieStore.get("session");
+
+  const isLoggedIn = await isLoggedInServerSide(sessionCookie?.value);
+  if (!isLoggedIn) {
+    redirect("/login");
+  }
+
   const data = await request("http://localhost:8080/query", queryDefinition);
   return <main>{data && <TweetColumn fragment={data} />}</main>;
 }
