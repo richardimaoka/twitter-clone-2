4d8af709e04384a094863f535a32e6a0d3e87a7c
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-22 10:15:27 +0900

    replace div with form

--- next/components/thread/reactions/Reactions.tsx
+++ next/components/thread/reactions/Reactions.tsx
@@ -24,12 +24,12 @@ export const Reactions = (props: Props) => {
   const fragment = useFragment(fragmentDefinition, props.fragment);
 
   return (
-    <div className={styles.component}>
+    <form className={styles.component}>
       <ReplyButton counts={fragment.numReplies} />
       <RetweetyButton counts={fragment.numRetweets} />
       <LikeButton counts={fragment.numLikes} />
       <BookmarkButton counts={fragment.numBookmarks} />
       <ShareButton />
-    </div>
+    </form>
   );
 };
