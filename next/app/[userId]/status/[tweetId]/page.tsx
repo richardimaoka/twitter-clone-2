126866eab30bd0fee39af4a6ece7e368d9d068d3
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-10-01 14:17:10 +0900

    tweet error handling

--- next/app/[userId]/status/[tweetId]/page.tsx
+++ next/app/[userId]/status/[tweetId]/page.tsx
@@ -2,7 +2,7 @@ import { LeftPane } from "@/components/leftpane/LeftPane";
 import { RightPane } from "@/components/rightpane/RightPane";
 import { TweetThread } from "@/components/thread/thread/TweetThread";
 import { graphql } from "@/libs/gql";
-import { GraphQLClient, request } from "graphql-request";
+import { ClientError, GraphQLClient, request } from "graphql-request";
 import { cookies } from "next/headers";
 import styles from "./style.module.css";
 // import { print } from "graphql";
@@ -22,16 +22,28 @@ export default async function Page() {
     headers: { cookie: cookieStore.toString() },
   });
 
-  const data = await client.request(queryDefinition);
-  return (
-    <div className={styles.home}>
-      <section className={styles.leftPane}>
-        <LeftPane />
-      </section>
-      <main>{data && <TweetThread fragment={data} />}</main>
-      <section className={styles.rightPane}>
-        <RightPane />
-      </section>
-    </div>
-  );
+  try {
+    const data = await client.request(queryDefinition);
+    return (
+      <div className={styles.home}>
+        <section className={styles.leftPane}>
+          <LeftPane />
+        </section>
+        <main>{data && <TweetThread fragment={data} />}</main>
+        <section className={styles.rightPane}>
+          <RightPane />
+        </section>
+      </div>
+    );
+  } catch (e) {
+    //TODO: not found implementation, and separation of not-found from UI logic
+    //  https://nextjs.org/docs/app/building-your-application/routing
+    //  https://nextjs.org/docs/app/api-reference/file-conventions/not-found
+    //  https://nextjs.org/docs/app/api-reference/functions/not-found
+    let message = "unknown error";
+    if (e instanceof ClientError && e.response.errors)
+      message = e.response.errors[0].message;
+
+    return <>{message}</>;
+  }
 }
